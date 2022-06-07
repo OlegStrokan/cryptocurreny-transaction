f785358ebf2a66b2451346483d787d5be221122e
olehstrokan <oleh@wattstor.com>
2022-06-07 11:09:07 +0200

    Added send transaction method

--- frontend/src/context/TransactionContext.tsx
+++ frontend/src/context/TransactionContext.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {ethers } from 'ethers'
+import { ethers } from 'ethers'
 import { contractABI, contractAddress } from '../utils/constans';
+
 export const TransactionContext = React.createContext(null as any);
 // @ts-ignore
 const { ethereum } = window;
@@ -8,9 +9,7 @@ const { ethereum } = window;
 const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
-  const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-  return { provider, signer, transactionContract }
+  return new ethers.Contract(contractAddress, contractABI, signer)
 }
 
 
@@ -54,6 +53,31 @@ export const TransactionProvider =  ({ children }: any) => {
 
   }
 
+  const sendTransaction = async () => {
+    try {
+      if (!ethereum) return alert('Please install metamask');
+
+      const { addressTo, amount, keyword, message} = formData;
+      const transactionContract = getEthereumContract();
+
+      const parseAmount = ethers.utils.parseEther(amount)
+
+      await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [{
+          from: currentAccount,
+          to: addressTo,
+          gas: '0x5208',
+          value: parseAmount._hex,
+        }]
+      })
+
+      const transactionHash = await transactionContract.addToBlockchain(addressTo, parseAmount, message, keyword);
+    } catch (e) {
+      throw new Error('No ethereum object')
+    }
+  }
+
 
 
     React.useEffect(() => {
@@ -63,7 +87,7 @@ export const TransactionProvider =  ({ children }: any) => {
 
 
   return (
-    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange } as any}>
+    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction } as any}>
       {children}
     </TransactionContext.Provider>
   )
