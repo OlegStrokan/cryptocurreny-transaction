aaa2b0b88b09232a3e935d741247d7fab7fc1cff
olehstrokan <oleh@wattstor.com>
2022-06-08 10:25:19 +0200

    Added loading functional to context, added dummy data, updated transaction component

--- frontend/src/components/Transactions.tsx
+++ frontend/src/components/Transactions.tsx
@@ -1,8 +1,11 @@
-import React, { useContext } from "react";
+import React from 'react';
+import useFetch from "../hooks/useFetch";
+import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
+import { TransactionContext } from '../context/TransactionContext';
 
-
-
-const TransactionsCard = () => {
+const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }: any) => {
+  const gifUrl = useFetch({ keyword });
 
   return (
     <div className="bg-[#181918] m-4 flex flex-1
@@ -15,25 +18,27 @@ const TransactionsCard = () => {
     >
       <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
-          <a href={`https://ropsten.etherscan.io/address/`} target="_blank" rel="noreferrer">
-            <p className="text-white text-base">From:</p>
+          <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+            <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
           </a>
-          <a href={`https://ropsten.etherscan.io/address/`} target="_blank" rel="noreferrer">
-            <p className="text-white text-base">To:</p>
+          <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
+            <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
           </a>
-          <p className="text-white text-base">Amount:  ETH</p>
+          <p className="text-white text-base">Amount: {amount} ETH</p>
+          {message && (
             <>
               <br />
-              <p className="text-white text-base">Message</p>
+              <p className="text-white text-base">Message: {message}</p>
             </>
-          )
+          )}
         </div>
         <img
+          src={gifUrl || url}
           alt="nature"
           className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
         />
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-          <p className="text-[#37c7da] font-bold"></p>
+          <p className="text-[#37c7da] font-bold">{timestamp}</p>
         </div>
       </div>
     </div>
@@ -41,21 +46,26 @@ const TransactionsCard = () => {
 };
 
 const Transactions = () => {
+  const { transactions, currentAccount } = React.useContext(TransactionContext);
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount ? (
           <h3 className="text-white text-3xl text-center my-2">
             Latest Transactions
           </h3>
+        ) : (
           <h3 className="text-white text-3xl text-center my-2">
             Connect your account to see the latest transactions
           </h3>
-        )
+        )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
-
-      </div>
+          {[...dummyData, ...transactions].reverse().map((transaction, i) => (
+            <TransactionsCard key={i} {...transaction} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- frontend/src/context/TransactionContext.tsx
+++ frontend/src/context/TransactionContext.tsx
@@ -13,16 +13,19 @@ const getEthereumContract = () => {
 }
 
 
-export const TransactionProvider =  ({ children }: any) => {
+export const TransactionProvider = ({ children }: any) => {
 
   const [currentAccount, setCurrentAccount] = React.useState('');
-  const [formData, setFormData] = React.useState({ addressTo: '', amount: '', keyword: '', message: ''});
+  const [formData, setFormData] = React.useState({ addressTo: '', amount: '', keyword: '', message: '' });
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  // @ts-ignore
+  const [transactionCount, setTransactionCount] = React.useState<boolean>(localStorage.getItem('transactionCount'));
 
   const handleChange = (e: any, name: any) => {
-    setFormData((prevState) => ({ ...prevState, [name]: e.target.value}))
+    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }))
   }
 
-  const checkIfWalletIsConnected = async () => {
+  const checkIfWalletIsConnected = async() => {
     try {
       if (!ethereum) return alert('Please install metamask');
 
@@ -33,31 +36,31 @@ export const TransactionProvider =  ({ children }: any) => {
       } else {
 
       }
-    }
-    catch (e) {
-
+    } catch (e) {
+      throw new Error('No ethereum object')
     }
   }
 
-  const connectWallet = async () => {
+  const connectWallet = async() => {
     try {
       if (!ethereum) return alert('Please install metamask');
 
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 
       setCurrentAccount(accounts[0])
+      window.location.reload();
 
     } catch (e) {
-        throw new Error('No ethereum object')
+      throw new Error('No ethereum object')
     }
 
   }
 
-  const sendTransaction = async () => {
+  const sendTransaction = async() => {
     try {
       if (!ethereum) return alert('Please install metamask');
 
-      const { addressTo, amount, keyword, message} = formData;
+      const { addressTo, amount, keyword, message } = formData;
       const transactionContract = getEthereumContract();
 
       const parseAmount = ethers.utils.parseEther(amount)
@@ -73,21 +76,33 @@ export const TransactionProvider =  ({ children }: any) => {
       })
 
       const transactionHash = await transactionContract.addToBlockchain(addressTo, parseAmount, message, keyword);
+
+      setIsLoading(true);
+
+      console.log(`Loading - ${transactionHash.hash}`)
+
+      await transactionHash.wait();
+
+      setIsLoading(false);
+      console.log(`Success - ${transactionHash.hash}`)
+
+      const transactionCount = await transactionContract.getTransactionCount();
+
+      setTransactionCount(transactionCount.toNumber())
     } catch (e) {
       throw new Error('No ethereum object')
     }
   }
 
 
-
-    React.useEffect(() => {
-      checkIfWalletIsConnected()
-    }, [])
-
+  React.useEffect(() => {
+     checkIfWalletIsConnected()
+  }, [])
 
 
   return (
-    <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction } as any}>
+    <TransactionContext.Provider
+      value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction } as any}>
       {children}
     </TransactionContext.Provider>
   )
